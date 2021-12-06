8c4bf236d86fe5d9da1e55f395ea1f4d1c05ac2c
f-web14usr <92645376+f-web14usr@users.noreply.github.com>
2021-12-06 21:41:55 +0330

    template card added
    فایل productCard  در پوشه تمپلیت برای ای جاد کارت ها به صورت include ایجادشد

---
+++ templates/productCard.php
@@ -0,0 +1,17 @@
+<?php
+echo "
+<article class='card filled-card'>
+
+   
+<img src='{$product['p_image']}' class='card-img-top'>
+
+<div class='card-body' >
+<h5 class='card-title'>{$product['p_name']} </h5>
+<p class='card-text'>{$product['body_text']}</p>
+<h3 class='product-card-price'>{$product['price']} </h3>
+<a href='productDetails_controller.php?id={$product['id']}'  class='btn card-btn primary  '>اطلاعات بیشتر</a>
+
+</div>
+</article>	
+";
+?>

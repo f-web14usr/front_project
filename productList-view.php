4f2334cca970bec45290b3e8d53519aab169d9ed
f-web14usr <92645376+f-web14usr@users.noreply.github.com>
2021-12-13 15:05:51 +0330

    صفحه کاتالوگ محصولات داخل گرید لایوت صفحه

--- productList-view.php
+++ productList-view.php
@@ -5,16 +5,42 @@
         <meta charset="utf-8">
         <link rel="stylesheet" href="css/style.css" type="text/css">
     </head>
-    <body>
+    <body class="grid-layout">
+    
+    <header>کاتالوگ محصولات</header>
+    <aside>
+
+   بخش کناری
+
+</aside>
+    <main>
+     <nav>
+ منو نویگیشن
+
+     </nav>
+     
+    </main>
+
+
+
+
+     <section>
    <div class="card-group"> 
   <?php
         
     foreach( $productList as $product ){
        include 'templates/productCard.php';
     }
-  
+    
   ?>
       </div>
-       
+      </section>
+
+    
+
+      <footer>
+  فوتر
+      </footer>
+     
     </body>
 </html>

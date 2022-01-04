218d3a92fc54a5f5db68e24913cc36e2486f3c36
f-web14usr <92645376+f-web14usr@users.noreply.github.com>
2022-01-04 18:33:31 +0330

    ajax added

--- productCard.php
+++ productCard.php
@@ -1,4 +1,15 @@
 <?php
+/*
+$uid=Authentication :: uid();
+$pid=$product['id'];
+
+$sql="SELECT * FROM likeproduct WHERE uid=? AND pid=?";
+$result=$db->query($sql,$uid,$pid);
+$likeicon=$result ->fetchArray();
+
+//سلکت کن از جدول لایک محصول آن  ردیفی راکه کاربر با شناسه فلان ان  محصول را  لایک کرده 
+*/
+
 echo "
 <article class='card filled-card' >
 
@@ -9,18 +20,36 @@ echo "
 <h5 class='card-title'>{$product['p_name']} </h5>
 <p class='card-text'>{$product['body_text']}</p>
 <h3 class='product-card-price'>{$product['price']} </h3>
-<a href='productDetails_controller.php?id={$product['id']}'  class='btn card-btn primary link ' >اطلاعات بیشتر</a>
+<a href='productDetails_controller.php?id={$product['id']}'  class='btn btn-text-button ' >اطلاعات بیشتر</a>
 <div class='card-buttom'>
-<a href='likeProduct-controler.php?id={$product['id']}' title='ابتدا باید لاگین کنید'>
-<i class='   far  fa-heart icon-btn  '></i>
-</a>
-<a href='basketProduct-controler.php?id={$product['id']} '>
 
-<i class='fas fa-shopping-cart  icon-btn   '></i>
-</a>
-<a href='bookmarkProduct-controler.php?id={$product['id']} '>
+<a href='basketProduct-controler.php?id={$product['id']} ' class='btn card-btn btn-outlined-button'>
+<i class='fas fa-shopping-cart'></i>
+افزودن به سبد خرید
 
-<i class='far fa-bookmark  icon-btn'></i>
+</a>
+<br>
+
+<a href='likeProduct-controler.php?id={$product['id']}' title='ابتدا باید لاگین کنید'class='icon-btn'>
+";
+//اگر ردیفی وجود داشت آیکن لایک را به شکل تو پر نشان بده
+/*
+if(isset($likeicon['id'])){//قبلا لایک کرده
+echo "<i class='   fas  fa-heart  like '></i>";
+}
+else{//درغیراین صورت  به شکل تو خالی نشان بده 
+    */
+    echo "<i class='   far  fa-heart   '></i>";
+   
+//}
+echo"
+</a>
+
+
+
+<a href='bookmarkProduct-controler.php?id={$product['id']} 'class='icon-btn'>
+
+<i class='far fa-bookmark  '></i>
 </a>
 
 
